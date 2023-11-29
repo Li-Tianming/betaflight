0ff3795db472fcb8668d704eae70eae214c90957
Li <li@li.com>
2023-11-29 15:56:54 +0800

    Adapt for customized apm32f103 flight controller

--- src/main/target/ALIENFLIGHTF1/target.h
+++ src/main/target/ALIENFLIGHTF1/target.h
@@ -22,45 +22,66 @@
 
 #define BRUSHED_ESC_AUTODETECT
 
-#define LED0_PIN                PB3
-#define LED1_PIN                PB4
+#define LED0_PIN                PC13//PB3
+#define LED1_PIN                PC13//PB4
 
-#define BEEPER                  PA12
+//#define BEEPER                  PA12
 
-#define USE_EXTI
-#define MAG_INT_EXTI            PC14
+//#define USE_EXTI
+//#define MAG_INT_EXTI            PC14
 #define USE_MPU_DATA_READY_SIGNAL
 //#define DEBUG_MPU_DATA_READY_INTERRUPT
 
-#define GYRO
-#define USE_GYRO_MPU6050
+// #define SPI1_NSS_PIN          PB13
 
-#define GYRO_MPU6050_ALIGN      CW0_DEG
+#define MPU6500_CS_PIN          PB13
+#define MPU6500_SPI_INSTANCE    SPI1
+
+#define GYRO
+#define USE_GYRO_SPI_MPU6500
+
+#define GYRO_MPU6500_ALIGN      CW270_DEG
 
 #define ACC
-#define USE_ACC_MPU6050
+#define USE_ACC_SPI_MPU6500
 
-#define ACC_MPU6050_ALIGN       CW0_DEG
+#define ACC_MPU6500_ALIGN       CW270_DEG
+
+
+#define USE_SPI
+#define USE_SPI_DEVICE_1
+
+#define SPI1_SCK_PIN            PA5
+#define SPI1_MISO_PIN           PA6
+#define SPI1_MOSI_PIN           PA7
+
+
 
 #define USE_UART1
 #define USE_UART2
-#define USE_UART3
-#define USE_SOFTSERIAL1
-#define USE_SOFTSERIAL2
-#define SERIAL_PORT_COUNT       5
+// #define USE_UART3
+// #define USE_SOFTSERIAL1
+// #define USE_SOFTSERIAL2
+#define SERIAL_PORT_COUNT       2
 
-#define UART3_RX_PIN            PB11
-#define UART3_TX_PIN            PB10
+#define UART1_TX_PIN            PA9
+#define UART1_RX_PIN            PA10
 
-#define USE_I2C
-#define USE_I2C_DEVICE_2
-#define I2C_DEVICE              (I2CDEV_2)
+#define UART2_TX_PIN            PA2
+#define UART2_RX_PIN            PA3
+
+// #define UART3_RX_PIN            PB11
+// #define UART3_TX_PIN            PB10
+
+// #define USE_I2C
+// #define USE_I2C_DEVICE_2
+// #define I2C_DEVICE              (I2CDEV_2)
 
 #define USE_ADC
 #define CURRENT_METER_ADC_PIN   PB1
 #define VBAT_ADC_PIN            PA4
 #define RSSI_ADC_PIN            PA1
-#define EXTERNAL1_ADC_PIN       PA5
+// #define EXTERNAL1_ADC_PIN       PA5
 
 #define SPEKTRUM_BIND
 #define USE_SPEKTRUM_BIND_PLUG
@@ -68,11 +89,17 @@
 
 #define DEFAULT_FEATURES        FEATURE_MOTOR_STOP
 #define DEFAULT_RX_FEATURE      FEATURE_RX_SERIAL
-#define SERIALRX_PROVIDER       SERIALRX_SPEKTRUM2048
+#define SERIALRX_PROVIDER       SERIALRX_SBUS
 #define SERIALRX_UART           SERIAL_PORT_USART2
-#define RX_CHANNELS_TAER
+#define RX_CHANNELS_AERT
 
 #define USE_SERIAL_4WAY_BLHELI_INTERFACE
+
+// #define BARO
+// #define USE_BARO_SPI_MS5611
+
+// #define MS5611_CS_PIN           PB15
+// #define MS5611_SPI_INSTANCE     SPI1
 
 // IO - assuming all IOs on 48pin package
 #define TARGET_IO_PORTA         0xffff
--- src/main/target/ALIENFLIGHTF1/target.mk
+++ src/main/target/ALIENFLIGHTF1/target.mk
@@ -1,5 +1,7 @@
 F1_TARGETS  += $(TARGET)
 
 TARGET_SRC = \
+            drivers/accgyro/accgyro_mpu6500.c \
             drivers/accgyro/accgyro_mpu.c \
-            drivers/accgyro/accgyro_mpu6050.c
+            drivers/accgyro/accgyro_spi_mpu6500.c\
+            drivers/barometer/barometer_ms5611.c
